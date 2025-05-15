c0e49cb2e8779e01c5a3eb33ffab5aebf6c93956
Laura Wendemuth <laura.wendemuth@obsbs.de>
2025-05-15 09:16:23 +0200

    Schriftart geändert und Tabs implementiert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from './services/pokemon.service';
 import { NgIf } from '@angular/common';
+import { HeaderComponent } from './header/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
-  
+  title = 'WhosThatPokemon';
   constructor(public pokemonService: PokemonService){}
 
   ngOnInit(): void{
